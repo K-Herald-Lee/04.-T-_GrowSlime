86bdea6a6164b597c652a7b1701a02bb874bb846
K-Herald-Lee <herald.lee1118@gmail.com>
2022-11-24 00:52:19 -0500

    Create purchasing system
    https://github.com/K-Herald-Lee/04.-T-_GrowSlime/issues/9

---
+++ GrowSlime/Assets/02.Scripts/Jelly.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jelly
+{
+    public int ID;
+    public int level;
+    public float Exp;
+
+    public Jelly(int ID, int level, float Exp)
+    {
+        this.ID = ID;
+        this.level = level;
+        this.Exp = Exp;
+    }
+}
